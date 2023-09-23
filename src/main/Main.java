5564767c49432dde68418b57725ab9930e513d34
Aloysius <aloysius.tan@dxc.com>
2023-09-23 11:39:10 +0800

    add bye

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bye!");
     }
 
     public int multiply(int num1 , int num2,int num3){
