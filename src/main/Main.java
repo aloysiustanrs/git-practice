e9011f6e84add2ac74f15ac45f0d014c14ae4205
Aloysius Tan <aloysiustanrs@gmail.com>
2023-09-25 23:52:20 +0800

    change sum method in Main to have 3 arguments

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public int sum(int num1 , int num2){
-        return num1 + num2;
+    public int sum(int num1 , int num2,int num3){
+        return num1 + num2 + num3;
     }
 }
