274cf2e8c3daeb1bdca2cd9546f2a08c685e88a1
Aloysius Tan <aloysiustanrs@gmail.com>
2023-09-25 23:24:05 +0800

    rebase : add Dog class

--- src/main/Dog.java
+++ src/main/Dog.java
@@ -1,12 +1,13 @@
 package main;
 
 public class Dog {
-
+    
     private String name;
     private int age;
 
     public Dog(String name, int age) {
         this.name = name;
         this.age = age;
+
     }
 }
