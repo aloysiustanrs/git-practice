c4d34f0d71093f5950d1ab5559425ce255cd3259
Aloysius Tan <aloysiustanrs@gmail.com>
2023-09-30 17:30:57 +0800

    change sum method to multiply

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public int sum(int num1 , int num2,int num3){
-        return num1 + num2 + num3;
+    public int multiply(int num1 , int num2,int num3){
+        return num1 * num2 * num3;
     }
 }
